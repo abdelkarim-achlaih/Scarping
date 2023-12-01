9d43268a1bcbbfa386a9f7de1d0558fd096fa787
Abdelkarim Achlaih <abdelkarima46@gmail.com>
2023-12-01 01:03:24 +0100

    extractAgencyName

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 const xlsx = require("xlsx");
 const puppeteer = require("puppeteer");
-const fs = require("fs");
+
+// Example usage
+const baseUrl =
+	"https://www.google.com/localservices/prolist?g2lbs=ANTchaO9bplSST1oDj2TNxdveYvPKk3czDpsN1oeg3ciJjEbP4YM67F54Ysxg8m9u4mb9Ir2y9tTMWKw3t6RO7QYxn0un9ogyFKpsmKwS1l46mrM_IYW2G4TOnqdZrjedTY3jcmFETa-Ukdf9UvWYPoK9EoxhsqGQg%3D%3D&hl=en-MA&gl=ma&cs=1&ssta=1&q=media%20agencies%20in%20dubai&oq=media%20agencies%20in%20dubai&slp=MgA6HENoTUk1WmlNaktyc2dnTVYySjlvQ1IySHJRTWJSAggCYACSAa8CCgsvZy8xdGZweTl6ZgoNL2cvMTFmZ2tueHYwYgoML2cvMTFoMXIzcXcyCg0vZy8xMWoxZmRrNjlyCg0vZy8xMWhfd3kyNjI1Cg0vZy8xMWYxMjcwNXF5Cg0vZy8xMWJ0X253Z2hfCg0vZy8xMWI2aF9kdnM1CgsvZy8xdGo2czliZgoML2cvMXB0eXFuczZ2CgsvZy8xdGZtbHc0NwoNL2cvMTFqN2tjNjF0MAoNL2cvMTFma2pkNHh3NgoML2cvMWhjYmgzaDE3Cg0vZy8xMWh6d2QxeHBxCg0vZy8xMWcxOXY1c3Y1Cg0vZy8xMWZfZDA0ZjJuCg0vZy8xMWtjeDk2bXZ6Cg0vZy8xMWwzY2QwMDZnCg0vZy8xMWNteXlkc3luEgQSAggBEgQKAggBmgEGCgIXGRAA&src=2&serdesk=1&sa=X&ved=2ahUKEwiKwoaMquyCAxXVfKQEHS_ABWcQjGp6BAgnEAE&scp=ChdnY2lkOmFkdmVydGlzaW5nX2FnZW5jeRJeEhIJRcbZaklDXz4RYlEphFBu5r0aEglFxtlqSUNfPhHpKQArv9DOdCIcRHViYWkgLSBVbml0ZWQgQXJhYiBFbWlyYXRlcyoUDXovxw4VX563IB3HaB0PJSB5HiEwABoObWVkaWEgYWdlbmNpZXMiF21lZGlhIGFnZW5jaWVzIGluIGR1YmFpKhJBZHZlcnRpc2luZyBhZ2VuY3k%3D"; // Replace with your desired URL
+const excelFileName = "output.xlsx";
+const city = "Dubai"; // Replace with the desired city
+const country = "United Arab Emirates"; // Replace with the desired country
+
+displayMediaAgenciesAndEmails(baseUrl, excelFileName, city, country);
 
 // Function to extract website URLs from a given webpage
 async function extractWebsiteUrls(baseUrl) {
@@ -92,21 +100,33 @@ async function extractEmail(url) {
 		return [];
 	}
 }
+// Function to extract agency name from the URL
+function extractAgencyName(url) {
+	// Remove "www." if it exists, then split by dots
+	const urlWithoutWWW = url.replace("www.", "");
+	const urlParts = urlWithoutWWW.split(".");
 
-async function displayMediaAgenciesAndEmails(baseUrl, excelFileName) {
+	// Assuming the agency name is the second part of the URL
+	return urlParts.length >= 2 ? urlParts[1] : "Unknown";
+}
+
+async function displayMediaAgenciesAndEmails(
+	baseUrl,
+	excelFileName,
+	city,
+	country
+) {
 	// Extract and display website URLs
 	const websiteUrls = await extractWebsiteUrls(baseUrl);
-	// console.log("Website URLs:", websiteUrls);
 
 	// Extract and display emails from each website URL
 	const data = [];
 	for (const url of websiteUrls) {
 		const emails = await extractEmail(url);
-		const agencyName = url.split(".")[1]; // Extract agency name from the URL (for demonstration purposes)
+		const agencyName = extractAgencyName(url); // Extract agency name from the URL (for demonstration purposes)
 
 		// Push data to the array
-		data.push({ agencyName, url, emails: emails.join(", ") });
-		// console.log(`${agencyName}: ${emails.join(", ")}`);
+		data.push({ agencyName, url, city, country, emails: emails.join(", ") });
 	}
 
 	// Export data to an Excel file
@@ -135,10 +155,3 @@ async function exportToExcel(data, excelFileName) {
 		console.error("Error exporting data to Excel file:", error.message);
 	}
 }
-
-// Example usage
-const baseUrl =
-	"https://www.google.com/localservices/prolist?g2lbs=ANTchaO9bplSST1oDj2TNxdveYvPKk3czDpsN1oeg3ciJjEbP4YM67F54Ysxg8m9u4mb9Ir2y9tTMWKw3t6RO7QYxn0un9ogyFKpsmKwS1l46mrM_IYW2G4TOnqdZrjedTY3jcmFETa-Ukdf9UvWYPoK9EoxhsqGQg%3D%3D&hl=en-MA&gl=ma&cs=1&ssta=1&q=media%20agencies%20in%20dubai&oq=media%20agencies%20in%20dubai&slp=MgA6HENoTUk1WmlNaktyc2dnTVYySjlvQ1IySHJRTWJSAggCYACSAa8CCgsvZy8xdGZweTl6ZgoNL2cvMTFmZ2tueHYwYgoML2cvMTFoMXIzcXcyCg0vZy8xMWoxZmRrNjlyCg0vZy8xMWhfd3kyNjI1Cg0vZy8xMWYxMjcwNXF5Cg0vZy8xMWJ0X253Z2hfCg0vZy8xMWI2aF9kdnM1CgsvZy8xdGo2czliZgoML2cvMXB0eXFuczZ2CgsvZy8xdGZtbHc0NwoNL2cvMTFqN2tjNjF0MAoNL2cvMTFma2pkNHh3NgoML2cvMWhjYmgzaDE3Cg0vZy8xMWh6d2QxeHBxCg0vZy8xMWcxOXY1c3Y1Cg0vZy8xMWZfZDA0ZjJuCg0vZy8xMWtjeDk2bXZ6Cg0vZy8xMWwzY2QwMDZnCg0vZy8xMWNteXlkc3luEgQSAggBEgQKAggBmgEGCgIXGRAA&src=2&serdesk=1&sa=X&ved=2ahUKEwiKwoaMquyCAxXVfKQEHS_ABWcQjGp6BAgnEAE&scp=ChdnY2lkOmFkdmVydGlzaW5nX2FnZW5jeRJeEhIJRcbZaklDXz4RYlEphFBu5r0aEglFxtlqSUNfPhHpKQArv9DOdCIcRHViYWkgLSBVbml0ZWQgQXJhYiBFbWlyYXRlcyoUDXovxw4VX563IB3HaB0PJSB5HiEwABoObWVkaWEgYWdlbmNpZXMiF21lZGlhIGFnZW5jaWVzIGluIGR1YmFpKhJBZHZlcnRpc2luZyBhZ2VuY3k%3D"; // Replace with your desired URL
-const excelFileName = "output.xlsx";
-
-displayMediaAgenciesAndEmails(baseUrl, excelFileName);
